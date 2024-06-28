69882ceaaf42655e626cbe1da5e3159a9b4d354d
goroshnikov.p <goroshnikov.p@dobrozaim.ru>
2024-06-28 16:18:07 +0300

    работа с комнатами

--- utils.js
+++ utils.js
@@ -1,4 +1,15 @@
 const trimStr = (str) =>
   str && typeof str === "string" ? str.trim().toLowerCase() : "";
 
+const logWithTime = (message, ...args) => {
+    const date = new Date();
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    const formatedTime = `${h}:${m}:${s}`;
+
+    console.log(formatedTime, message, ...args);
+}
+
 exports.trimStr = trimStr;
+exports.logWithTime = logWithTime;
